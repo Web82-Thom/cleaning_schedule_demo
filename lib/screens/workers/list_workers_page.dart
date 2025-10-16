7bca9a3515b22e022e391973ad72df44543de1db
unknown <thom.orta@gmail.com>
2025-10-16 18:58:15 +0200

    add safearea

--- lib/screens/workers/list_workers_page.dart
+++ lib/screens/workers/list_workers_page.dart
@@ -28,96 +28,98 @@ class _ListWorkersPageState extends State<ListWorkersPage> {
   
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Liste des travailleurs")),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: workersRef.orderBy('firstName', descending: false).snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          final docs = snapshot.data?.docs ?? [];
-
-          if (docs.isEmpty) {
-            return const Center(child: Text('Aucun travailleur trouvé.'));
-          }
-
-          return ListView.builder(
-            padding: const EdgeInsets.all(8),
-            itemCount: docs.length,
-            itemBuilder: (context, index) {
-              final data = docs[index].data() as Map<String, dynamic>;
-              final id = docs[index].id;
-
-              final status = workersController.getStatusLabel(data);
-              final isAbsentData = data['isAbcent'] == true;
-
-              return InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (_) => DetailsWorkerPage(workerId: id)),
-                  );
-                },
-                child: Card(
-                  margin: const EdgeInsets.symmetric(vertical: 6),
-                
-                  child: ListTile(
-                    title: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          child: Text(
-                            '${data['firstName'] ?? ''} ${data['name'] ?? ''}',
-                            style: const TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        // Icône horloge si le worker a un workSchedule
-                        if (data['workSchedule'] != null)
-                        const Icon(
-                          Icons.schedule,
-                          color: Colors.pinkAccent,
-                          size: 20,
-                        ),
-                      ],
-                    ),
-                    subtitle: RichText(
-                      text: TextSpan(
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: const Text("Liste des travailleurs")),
+        body: StreamBuilder<QuerySnapshot>(
+          stream: workersRef.orderBy('firstName', descending: false).snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+      
+            final docs = snapshot.data?.docs ?? [];
+      
+            if (docs.isEmpty) {
+              return const Center(child: Text('Aucun travailleur trouvé.'));
+            }
+      
+            return ListView.builder(
+              padding: const EdgeInsets.all(8),
+              itemCount: docs.length,
+              itemBuilder: (context, index) {
+                final data = docs[index].data() as Map<String, dynamic>;
+                final id = docs[index].id;
+      
+                final status = workersController.getStatusLabel(data);
+                final isAbsentData = data['isAbcent'] == true;
+      
+                return InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (_) => DetailsWorkerPage(workerId: id)),
+                    );
+                  },
+                  child: Card(
+                    margin: const EdgeInsets.symmetric(vertical: 6),
+                  
+                    child: ListTile(
+                      title: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          if (!isAbsentData)
-                            TextSpan(
-                              text: status,
-                              style: TextStyle(
-                                color: workersController.getStatusColor(data),
-                                fontSize: 12,
-                              ),
+                          Expanded(
+                            child: Text(
+                              '${data['firstName'] ?? ''} ${data['name'] ?? ''}',
+                              style: const TextStyle(fontWeight: FontWeight.bold),
                             ),
-                          // Ajoute " / Absent" uniquement si le worker n'est pas en plein temps ou si absent
-                          if (isAbsentData) 
-                          const TextSpan(
-                            text: 'ABSENT',
-                            style: TextStyle(
-                              color: Colors.red,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 12,
-                            ),
+                          ),
+                          // Icône horloge si le worker a un workSchedule
+                          if (data['workSchedule'] != null)
+                          const Icon(
+                            Icons.schedule,
+                            color: Colors.pinkAccent,
+                            size: 20,
                           ),
                         ],
                       ),
+                      subtitle: RichText(
+                        text: TextSpan(
+                          children: [
+                            if (!isAbsentData)
+                              TextSpan(
+                                text: status,
+                                style: TextStyle(
+                                  color: workersController.getStatusColor(data),
+                                  fontSize: 12,
+                                ),
+                              ),
+                            // Ajoute " / Absent" uniquement si le worker n'est pas en plein temps ou si absent
+                            if (isAbsentData) 
+                            const TextSpan(
+                              text: 'ABSENT',
+                              style: TextStyle(
+                                color: Colors.red,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 12,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
-                ),
-              );
-            },
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () => workersController.showAddWorkerDialog(context),
-        backgroundColor: Colors.indigo,
-        icon: const Icon(Icons.add),
-        label: const Text("Ajouter", style: TextStyle(fontSize: 14)),
+                );
+              },
+            );
+          },
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () => workersController.showAddWorkerDialog(context),
+          backgroundColor: Colors.indigo,
+          icon: const Icon(Icons.add),
+          label: const Text("Ajouter", style: TextStyle(fontSize: 14)),
+        ),
       ),
     );
   }
